a3e80d52a24da2dfc44ec0175b644fdb43e6d74b
Sheehan Olver <solver@mac.com>
2019-08-06 18:13:09 +0100

    more * tests

--- test/test_matmul.jl
+++ test/test_matmul.jl
@@ -2,10 +2,23 @@ using QuasiArrays, Test
 import QuasiArrays: ApplyQuasiArray
 
 @testset "Multiplication" begin
-    A = QuasiDiagonal(Inclusion(0:0.1:1))
-    b = Inclusion(0:0.1:1)
-    Ab = A*b
-    @test Ab isa ApplyQuasiArray
-    @test Ab[0.1] ≈ 0.1^2
-    @test_throws DimensionMismatch A*Inclusion(1:2)
+    @testset "Diag * Inclusion" begin
+        A = QuasiDiagonal(Inclusion(0:0.1:1))
+        b = Inclusion(0:0.1:1)
+        Ab = A*b
+        @test Ab isa ApplyQuasiArray
+        @test Ab[0.1] ≈ 0.1^2
+        @test_throws DimensionMismatch A*Inclusion(1:2)
+    end
+    @testset "Quasi * Quasi" begin
+        A = QuasiArray(rand(3,3),(0:0.5:1,0:0.5:1))
+        @test A*A isa ApplyQuasiArray
+        @test QuasiArray(A*A) == QuasiArray(A.parent*A.parent,A.axes)
+    end
+    @testset "Quasi * Array" begin
+        A = QuasiArray(rand(3,3),(0:0.5:1,Base.OneTo(3)))
+        B = rand(3,3)
+        @test A*B isa ApplyQuasiArray
+        @test QuasiArray(A*B) == QuasiArray(A.parent*B,A.axes)
+    end
 end
