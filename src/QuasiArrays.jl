12bd48dde6da607acbfa8ac69583906d387f5703
Sheehan Olver <solver@mac.com>
2019-11-18 09:12:18 +0000

    Use sub_materialize

--- src/QuasiArrays.jl
+++ src/QuasiArrays.jl
@@ -37,7 +37,7 @@ import LazyArrays: MemoryLayout, UnknownLayout, Mul, ApplyLayout, BroadcastLayou
                     FlattenMulStyle, IdentityMulStyle, MulAddStyle, LazyArrayApplyStyle,
                     Applied, flatten, _flatten, arguments, _mat_mul_arguments, _vec_mul_arguments,
                     rowsupport, colsupport, tuple_type_memorylayouts, applylayout, broadcastlayout,
-                    LdivApplyStyle, most, InvLayout, PInvLayout,
+                    LdivApplyStyle, most, InvLayout, PInvLayout, sub_materialize,
                     _mul, rowsupport, DiagonalLayout, adjointlayout, transposelayout, conjlayout,
                     sublayout, call, combine_mul_styles, result_mul_style, LazyArrayStyle
 
