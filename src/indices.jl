d3c2d20627ca0d051eb74dc9f6f32657ceadc3f6
Sheehan Olver <solver@mac.com>
2019-11-18 11:19:24 +0000

    Update indices.jl

--- src/indices.jl
+++ src/indices.jl
@@ -161,6 +161,7 @@ getindex(S::Inclusion{T}, i::AbstractVector{<:Number}) where T =
     (@_inline_meta; @boundscheck checkbounds(S, i); convert(AbstractVector{T},i))
 getindex(S::Inclusion, i::Inclusion) =
     (@_inline_meta; @boundscheck checkbounds(S, i); copy(S))
+getindex(S::Inclusion, ::Colon) = copy(S)
 show(io::IO, r::Inclusion) = print(io, "Inclusion(", r.domain, ")")
 iterate(S::Inclusion, s...) = iterate(S.domain, s...)
 
