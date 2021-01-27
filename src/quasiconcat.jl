83df39c4c9244b3b6d1b22a5c3fcc7a299767b0a
Sheehan Olver <solver@mac.com>
2021-01-27 21:44:14 +0000

    Fix union of int domains

--- src/quasiconcat.jl
+++ src/quasiconcat.jl
@@ -17,7 +17,7 @@ function getindex(f::ApplyQuasiMatrix{T,typeof(hcat)}, k::Number, j::Number) whe
     throw(BoundsError(f, (k,j)))
 end
 
-union(x::Inclusion...) = Inclusion(UnionDomain(map(domain,x)...))
+union(x::Inclusion...) = Inclusion{mapreduce(eltype,promote_type,x)}(UnionDomain(map(domain,x)...))
 
 """
     UnionVcat
