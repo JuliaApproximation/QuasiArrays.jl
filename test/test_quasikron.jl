b879cea5978875547972d1df07835d37f20d9d8a
Sheehan Olver <solver@mac.com>
2021-07-05 13:04:46 +0100

    == for sub array (#73)
    * == for sub array
    
    * fix tests

--- test/test_quasikron.jl
+++ test/test_quasikron.jl
@@ -1,14 +1,14 @@
-using QuasiArrays, DomainSets, Test
+using QuasiArrays, DomainSets, StaticArrays, Test
 
 @testset "Kron" begin
     @testset "InclusionKron" begin
         a = Inclusion(0:0.5:2)
         b = Inclusion([1,3,4])
         c = quasikron(a,b)
-        @test eltype(c) == Tuple{Float64,Int}
-        @test c[(0.5,3)] == (0.5,3)
-        @test_throws BoundsError c[(0.6,3)]
-        @test_throws BoundsError c[(0.5,2)]
+        @test eltype(c) == SVector{2,Float64}
+        @test c[SVector(0.5,3)] == SVector(0.5,3)
+        @test_throws BoundsError c[SVector(0.6,3)]
+        @test_throws BoundsError c[SVector(0.5,2)]
     end
 
     @testset "QuasiKron" begin
@@ -16,8 +16,8 @@ using QuasiArrays, DomainSets, Test
         b = QuasiVector([5,6,8], [1,3,4])
         K = quasikron(a, b)
         @test axes(K) == (quasikron(axes(a,1),axes(b,1)),)
-        @test K[(0.5,3)] == a[0.5]b[3]
-        @test_throws BoundsError K[(0.6,3)]
-        @test_throws BoundsError K[(0.5,2)]
+        @test K[SVector(0.5,3)] == a[0.5]b[3]
+        @test_throws BoundsError K[SVector(0.6,3)]
+        @test_throws BoundsError K[SVector(0.5,2)]
     end
 end
